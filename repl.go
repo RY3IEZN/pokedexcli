4d3508485361ee624ba1d74b2c7b73c9171b06d1
unknown <unekue2@gmail.com>
2023-07-09 14:54:10 +0100

    makimg the terminal interactive

--- repl.go
+++ repl.go
@@ -12,17 +12,38 @@ func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print(" >")
+		// show a pointer to indicate to type
+		fmt.Print(" > ")
 
+		// intiate a scanner,get the input and point it to text
 		scanner.Scan()
 		text := scanner.Text()
 
+		// clean the input conv to lowercase
 		cleaned := clearInput(text)
+
+		// if the input is empty, loopback to the top
 		if len(cleaned) == 0 {
 			continue
 		}
 
-		fmt.Println("echoh,", cleaned)
+		// switch to check the command that was inputed
+		command := cleaned[0]
+		switch command {
+		case "help":
+			fmt.Println("Welcome pokedex help menu!")
+			fmt.Println("here are your available commands")
+			fmt.Println(" - help")
+			fmt.Println(" - exit")
+			fmt.Println("")
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid Command")
+		}
+
+		// print the word that was inputed
+		fmt.Println("echo,", cleaned)
 	}
 
 }
